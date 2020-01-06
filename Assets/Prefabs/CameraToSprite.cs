05d728f72bbe4ded821774640ce52c2fecb0acf7
paul <nlholdem@hotmail.com>
2020-01-06 22:08:36 +0000

    Hacked EnemyAudi to get the camera to render to a RenderTexture; we then read that it and log out the average greyscale to confirm that it does update each frame. Have created a material called MatCamera in the hope that we can apply this to a sprite and see what the camera sees - currently don't have a way to make this work though. The camera is a simple orthographic one, with the render volume set to be behind the enemy car - we seem to need this in order to render the other cars, but it does mean that the enemy car is in every frame. Needs a better solution!

---
+++ Assets/Prefabs/CameraToSprite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraToSprite : MonoBehaviour
+{
+    public SpriteRenderer spriteToUpdate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/EnemyCar.cs
+++ Assets/Scripts/EnemyCar.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class EnemyCar : MonoBehaviour {
+    public SpriteRenderer spriteRenderer;
 
     private Vector3 position;
     private readonly float carSpeed = -3;
@@ -15,8 +16,42 @@ public class EnemyCar : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         Camera cam = gameObject.GetComponent<Camera>();
+        cam.Render();
+        Texture2D tex = ToTexture2D(cam.activeTexture);
+        Color[] pixels = tex.GetPixels(0, 0, tex.width, tex.height);
+        float avg = 0.0F;
+        for (int i=0; i<tex.width; i++)
+        {
+            for (int j=0; j<tex.height; j++)
+            {
+                avg += pixels[i + tex.width * j].grayscale / (float)pixels.GetLength(0);
+            }
+        }
+
+        Debug.LogError("Avg: " + avg);
+        Sprite sprite = Sprite.Create(tex, new Rect(0,0,tex.width,tex.height),new Vector2(0.5F, 0.5F));
+        Sprite s = gameObject.GetComponent<Sprite>();
+
+        //Sprite sprite = Texture2DToSprite(tex);
+        //spriteRenderer.sprite = sprite;
         float x = Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
         float y = Input.GetAxis("Vertical") * carSpeed;
         transform.Translate (new Vector2 (x, (speed+y) * Time.deltaTime));
 	}
+
+    Texture2D ToTexture2D(RenderTexture rTex)
+    {
+        Texture2D tex = new Texture2D(512, 512, TextureFormat.RGB24, false);
+        RenderTexture.active = rTex;
+        tex.ReadPixels(new Rect(0, 0, rTex.width, rTex.height), 0, 0);
+        tex.Apply();
+        return tex;
+    }
+
+
+    Sprite Texture2DToSprite(Texture2D text2D)
+    {
+        Sprite sprite = Sprite.Create(text2D, new Rect(0, 0, text2D.width, text2D.height), Vector2.zero);
+        return sprite;
+    }
 }
